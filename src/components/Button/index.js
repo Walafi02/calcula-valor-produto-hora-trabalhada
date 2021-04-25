c12c927502d5234a9ba2047c68445539cbb89f28
Walafi Ferreira <walafif81@gmail.com>
2021-04-24 21:23:04 -0300

    configurando eslint, prettier e editorConfig

---
+++ src/components/Button/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+const Button = () => {
+  return (
+    <View>
+      <Text>Button</Text>
+    </View>
+  );
+};
+
+export default Button;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+import {Button} from './components';
+
+const Src = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>teste</Text>
+      <Button />
+    </View>
+  );
+};
+
+export default Src;
